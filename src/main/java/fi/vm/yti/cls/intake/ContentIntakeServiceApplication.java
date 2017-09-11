16ae9463b9d7508400f9709b14c4c8e534c467fa
Antti Tohmo <antti.tohmo@gofore.com>
2017-09-11 11:21:39 +0300

    Removing unused logger.

--- src/main/java/fi/vm/yti/cls/intake/ContentIntakeServiceApplication.java
+++ src/main/java/fi/vm/yti/cls/intake/ContentIntakeServiceApplication.java
@@ -1,7 +1,5 @@
 package fi.vm.yti.cls.intake;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -17,18 +15,14 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @ComponentScan({ "fi.vm.yti.cls.*" })
 public class ContentIntakeServiceApplication {
 
-    private static final Logger LOG = LoggerFactory.getLogger(ContentIntakeServiceApplication.class);
     private static boolean INITIALIZE_ON_STARTUP = true;
     private static boolean ONLY_INDEX = false;
     private static boolean YTI_ONLY = true;
 
     public static void main(final String[] args) {
         final ApplicationContext context = SpringApplication.run(ContentIntakeServiceApplication.class, args);
-
         final ServiceInitializer serviceInitializer = (ServiceInitializer) context.getBean(ServiceInitializer.class);
-
         serviceInitializer.printLogo();
-
         if (INITIALIZE_ON_STARTUP) {
             serviceInitializer.initialize(YTI_ONLY, ONLY_INDEX);
         }
