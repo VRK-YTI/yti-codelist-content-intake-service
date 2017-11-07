ad23c7c80023ffaf7f299e6f7ade6f8b626c30ae
Antti Tohmo <antti.tohmo@gofore.com>
2017-11-07 16:40:35 +0200

    Stub data population upon server startup refactored to ytitest content.

--- src/main/java/fi/vm/yti/codelist/intake/data/YtiDataAccess.java
+++ src/main/java/fi/vm/yti/codelist/intake/data/YtiDataAccess.java
@@ -34,10 +34,8 @@ import static fi.vm.yti.codelist.common.constants.ApiConstants.*;
 @Service
 public class YtiDataAccess {
 
-    private static final String DEFAULT_CODESCHEME_FILENAME = "v1_codeschemes.csv";
-    private static final String DEFAULT_CODEREGISTRY_FILENAME = "v1_coderegistries.csv";
-    private static final String DEFAULT_CODE_FILENAME = "v1_codes.csv";
-    private static final String DEFAULT_PROPERTYTYPE_FILENAME = "v1_propertytypes.csv";
+    private static final String DEFAULT_CODEREGISTRY_FILENAME = "coderegistries.csv";
+    private static final String DEFAULT_PROPERTYTYPE_FILENAME = "propertytypes.csv";
 
     private static final Logger LOG = LoggerFactory.getLogger(YtiDataAccess.class);
 
@@ -74,19 +72,20 @@ public class YtiDataAccess {
 
     public void initializeOrRefresh() {
         LOG.info("Initializing YTI DataAccess with mock/test data...");
-        loadDefaultCodeRegistries();
-        loadDefaultCodeSchemes();
-        loadDefaultCodes();
+        final List<CodeRegistry> codeRegistries = loadDefaultCodeRegistries();
+        final List<CodeScheme> codeSchemes = loadDefaultCodeSchemes(codeRegistries);
+        loadDefaultCodes(codeSchemes);
         loadDefaultPropertyTypes();
     }
 
-    private void loadDefaultCodeRegistries() {
+    private List<CodeRegistry> loadDefaultCodeRegistries() {
         LOG.info("Loading default CodeRegistries...");
+        final List<CodeRegistry> codeRegistries = new ArrayList<>();
         final Stopwatch watch = Stopwatch.createStarted();
         if (updateManager.shouldUpdateData(DATA_CODEREGISTRIES, DEFAULT_CODEREGISTRY_FILENAME)) {
             final UpdateStatus updateStatus = updateManager.createStatus(DATA_CODEREGISTRIES, SOURCE_INTERNAL, DEFAULT_CODEREGISTRY_FILENAME, UpdateManager.UPDATE_RUNNING);
             try (final InputStream inputStream = FileUtils.loadFileFromClassPath("/" + DATA_CODEREGISTRIES + "/" + DEFAULT_CODEREGISTRY_FILENAME);) {
-                final List<CodeRegistry> codeRegistries = codeRegistryParser.parseCodeRegistriesFromCsvInputStream(SOURCE_INTERNAL, inputStream);
+                codeRegistries.addAll(codeRegistryParser.parseCodeRegistriesFromCsvInputStream(SOURCE_INTERNAL, inputStream));
                 LOG.info("CodeRegistry data loaded: " + codeRegistries.size() + " CodeRegistries in " + watch);
                 watch.reset().start();
                 domain.persistCodeRegistries(codeRegistries);
@@ -101,79 +100,67 @@ public class YtiDataAccess {
         } else {
             LOG.info("CodeRegistries already up to date, skipping...");
         }
+        return codeRegistries;
     }
 
-    private void loadDefaultCodeSchemes() {
+    private List<CodeScheme> loadDefaultCodeSchemes(final List<CodeRegistry> codeRegistries) {
         LOG.info("Loading default CodeSchemes...");
+        final List<CodeScheme> codeSchemes = new ArrayList<>();
         final Stopwatch watch = Stopwatch.createStarted();
-        if (updateManager.shouldUpdateData(DATA_CODESCHEMES, DEFAULT_CODESCHEME_FILENAME)) {
-            final List<CodeScheme> codeSchemes = new ArrayList<>();
-            final UpdateStatus updateStatus = updateManager.createStatus(DATA_CODESCHEMES, SOURCE_INTERNAL, DEFAULT_CODESCHEME_FILENAME, UpdateManager.UPDATE_RUNNING);
-            final Set<CodeRegistry> defaultCodeRegistries = codeRegistryRepository.findAll();
-            defaultCodeRegistries.forEach(codeRegistry -> {
-                if (codeRegistry.getCodeValue().startsWith(DEFAULT_CODEREGISTRY_NAME_PREFIX)) {
-                    try (final InputStream inputStream = FileUtils.loadFileFromClassPath("/" + DATA_CODESCHEMES + "/" + DEFAULT_CODESCHEME_FILENAME);) {
-                        codeSchemes.addAll(codeSchemeParser.parseCodeSchemesFromCsvInputStream(codeRegistry, SOURCE_INTERNAL, inputStream));
-                    } catch (IOException e) {
-                        LOG.error("Issue with parsing CodeScheme file. Message: " + e.getMessage());
-                        updateManager.updateFailedStatus(updateStatus);
-                    } catch (Exception e) {
-                        LOG.error("Issue with existing CodeScheme found. Message: " + e.getMessage());
-                        updateManager.updateFailedStatus(updateStatus);
-                    }
-                }
-            });
-            if (updateStatus.getStatus().equals(UpdateManager.UPDATE_RUNNING)) {
-                LOG.info("CodeScheme data loaded: " + codeSchemes.size() + " CodeSchemes in " + watch);
-                watch.reset().start();
-                domain.persistCodeSchemes(codeSchemes);
-                LOG.info("CodeScheme data persisted in: " + watch);
-                updateManager.updateSuccessStatus(updateStatus);
-            }
-        } else {
-            LOG.info("CodeSchemes already up to date, skipping...");
-        }
-    }
-
-    private void loadDefaultCodes() {
-        LOG.info("Loading default Codes...");
-        final Stopwatch watch = Stopwatch.createStarted();
-        if (updateManager.shouldUpdateData(DATA_CODES, DEFAULT_CODE_FILENAME)) {
-            final List<Code> codes = new ArrayList<>();
-            final UpdateStatus updateStatus = updateManager.createStatus(DATA_CODES, SOURCE_INTERNAL, DEFAULT_CODE_FILENAME, UpdateManager.UPDATE_RUNNING);
-            final Set<CodeRegistry> defaultCodeRegistries = codeRegistryRepository.findAll();
-            defaultCodeRegistries.forEach(codeRegistry -> {
-                if (codeRegistry.getCodeValue().startsWith(DEFAULT_CODEREGISTRY_NAME_PREFIX)) {
-                    final Set<CodeScheme> defaultCodeSchemes = codeSchemeRepository.findByCodeRegistry(codeRegistry);
-                    defaultCodeSchemes.forEach(codeScheme -> {
-                        try (final InputStream inputStream = FileUtils.loadFileFromClassPath("/" + DATA_CODES + "/" + DEFAULT_CODE_FILENAME);) {
-                            codes.addAll(codeParser.parseCodesFromCsvInputStream(codeScheme, SOURCE_INTERNAL, inputStream));
-                        } catch (IOException e) {
-                            LOG.error("Issue with parsing Code file. Message: " + e.getMessage());
-                            updateManager.updateFailedStatus(updateStatus);
-                        } catch (Exception e) {
-                            LOG.error("Issue with existing Code found. Message: " + e.getMessage());
-                            updateManager.updateFailedStatus(updateStatus);
-                        }
-                    });
-                    if (defaultCodeSchemes.isEmpty()) {
-                        LOG.error("Loading default test CodeScheme with name: " + DEFAULT_CODESCHEME_NAME + " failed!");
-                        updateManager.updateFailedStatus(updateStatus);
-                    }
-                } else {
-                    LOG.error("Loading default test CodeRegistry with name: " + DEFAULT_CODEREGISTRY_NAME_PREFIX + " failed!");
+        codeRegistries.forEach(codeRegistry -> {
+            final String identifier = codeRegistry.getCodeValue();
+            if (updateManager.shouldUpdateData(DATA_CODESCHEMES, identifier)) {
+                final UpdateStatus updateStatus = updateManager.createStatus(DATA_CODESCHEMES, SOURCE_INTERNAL, identifier, UpdateManager.UPDATE_RUNNING);
+                try (final InputStream inputStream = FileUtils.loadFileFromClassPath("/" + DATA_CODESCHEMES + "/" + identifier + ".csv");) {
+                    codeSchemes.addAll(codeSchemeParser.parseCodeSchemesFromCsvInputStream(codeRegistry, SOURCE_INTERNAL, inputStream));
+                } catch (IOException e) {
+                    LOG.error("Issue with parsing CodeScheme file. Message: " + e.getMessage());
+                    updateManager.updateFailedStatus(updateStatus);
+                } catch (Exception e) {
+                    LOG.error("Issue with existing CodeScheme found. Message: " + e.getMessage());
                     updateManager.updateFailedStatus(updateStatus);
                 }
-            });
-            if (updateStatus.getStatus().equals(UpdateManager.UPDATE_RUNNING)) {
-                LOG.info("Code data loaded: " + codes.size() + " Codes in " + watch);
-                domain.persistCodes(codes);
-                LOG.info("Code data persisted in: " + watch);
-                updateManager.updateSuccessStatus(updateStatus);
+                if (updateStatus.getStatus().equals(UpdateManager.UPDATE_RUNNING)) {
+                    LOG.info("CodeScheme data loaded: " + codeSchemes.size() + " CodeSchemes in " + watch);
+                    watch.reset().start();
+                    domain.persistCodeSchemes(codeSchemes);
+                    LOG.info("CodeScheme data persisted in: " + watch);
+                    updateManager.updateSuccessStatus(updateStatus);
+                }
+            } else {
+                LOG.info("CodeSchemes already up to date, skipping...");
             }
-        } else {
-            LOG.info("Code already up to date, skipping...");
-        }
+        });
+        return codeSchemes;
+    }
+
+    private void loadDefaultCodes(final List<CodeScheme> codeSchemes) {
+        LOG.info("Loading default Codes...");
+        final Stopwatch watch = Stopwatch.createStarted();
+        codeSchemes.forEach(codeScheme -> {
+            final String identifier = codeScheme.getCodeRegistry().getCodeValue() + "_" + codeScheme.getCodeValue();
+            if (updateManager.shouldUpdateData(DATA_CODES, identifier)) {
+                final List<Code> codes = new ArrayList<>();
+                final UpdateStatus updateStatus = updateManager.createStatus(DATA_CODES, SOURCE_INTERNAL, identifier, UpdateManager.UPDATE_RUNNING);
+                try (final InputStream inputStream = FileUtils.loadFileFromClassPath("/" + DATA_CODES + "/" + identifier + ".csv");) {
+                    codes.addAll(codeParser.parseCodesFromCsvInputStream(codeScheme, SOURCE_INTERNAL, inputStream));
+                } catch (IOException e) {
+                    LOG.error("Issue with parsing Code file. Message: " + e.getMessage());
+                    updateManager.updateFailedStatus(updateStatus);
+                } catch (Exception e) {
+                    LOG.error("Issue with existing Code found. Message: " + e.getMessage());
+                    updateManager.updateFailedStatus(updateStatus);
+                }
+                if (updateStatus.getStatus().equals(UpdateManager.UPDATE_RUNNING)) {
+                    LOG.info("Code data loaded: " + codes.size() + " Codes in " + watch);
+                    domain.persistCodes(codes);
+                    LOG.info("Code data persisted in: " + watch);
+                    updateManager.updateSuccessStatus(updateStatus);
+                }
+            } else {
+                LOG.info("Code already up to date, skipping...");
+            }
+        });
     }
 
     private void loadDefaultPropertyTypes() {
