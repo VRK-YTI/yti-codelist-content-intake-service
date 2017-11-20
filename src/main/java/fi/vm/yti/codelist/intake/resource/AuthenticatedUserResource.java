22ed0d8cde7c1dc95f42e87b2690c85f99755a79
Antti Tohmo <antti.tohmo@gofore.com>
2017-11-20 16:13:42 +0200

    Added logging to authenticated-user API and corrected API name.

--- src/main/java/fi/vm/yti/codelist/intake/resource/AuthenticatedUserResource.java
+++ src/main/java/fi/vm/yti/codelist/intake/resource/AuthenticatedUserResource.java
@@ -5,19 +5,24 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import fi.vm.yti.security.AuthenticatedUserProvider;
 import fi.vm.yti.security.YtiUser;
 import io.swagger.annotations.Api;
+import static fi.vm.yti.codelist.common.constants.ApiConstants.API_PATH_AUTHENTICATED_USER;
+import static fi.vm.yti.codelist.common.constants.ApiConstants.METHOD_GET;
 
 @Component
 @Path("/authenticated-user")
-@Api(value = "authenticateduser", description = "Operation for fetching authenticated user to the frontend.")
+@Api(value = "authenticated-user", description = "Operation for fetching authenticated user to the frontend.")
 @Produces(MediaType.APPLICATION_JSON)
-public class AuthenticatedUserResource {
+public class AuthenticatedUserResource extends AbstractBaseResource {
 
+    private static final Logger LOG = LoggerFactory.getLogger(AuthenticatedUserResource.class);
     private final AuthenticatedUserProvider userProvider;
 
     @Autowired
@@ -27,6 +32,7 @@ public class AuthenticatedUserResource {
 
     @GET
     public YtiUser getAuthenticatedUser() {
+        logApiRequest(LOG, METHOD_GET, "", API_PATH_AUTHENTICATED_USER + "/");
         return this.userProvider.getUser();
     }
 }
