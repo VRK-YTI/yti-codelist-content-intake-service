a357ded3a9fd3517b7d9aa0c9bd2a8e98440a5d1
Antti Tohmo <antti.tohmo@gofore.com>
2018-01-11 15:35:18 +0200

    Bugfixing externalreference URI to proper path.

--- src/main/java/fi/vm/yti/codelist/intake/parser/ExternalReferenceParser.java
+++ src/main/java/fi/vm/yti/codelist/intake/parser/ExternalReferenceParser.java
@@ -181,7 +181,7 @@ public class ExternalReferenceParser extends AbstractBaseParser {
         String uri = null;
         if (id != null) {
             externalReference = externalReferenceRepository.findById(id);
-            uri = apiUtils.createResourceUri(API_PATH_PROPERTYTYPES, id.toString());
+            uri = apiUtils.createResourceUri(API_PATH_EXTERNALREFERENCES, id.toString());
         }
         if (externalReference != null) {
             boolean hasChanges = false;
@@ -226,7 +226,7 @@ public class ExternalReferenceParser extends AbstractBaseParser {
                 externalReference.setId(id);
             } else {
                 final UUID uuid = UUID.randomUUID();
-                uri = apiUtils.createResourceUri(API_PATH_PROPERTYTYPES, uuid.toString());
+                uri = apiUtils.createResourceUri(API_PATH_EXTERNALREFERENCES, uuid.toString());
                 externalReference.setId(uuid);
             }
             externalReference.setParentCodeScheme(parentCodeScheme);
