803e7af9952ca768f5449a01fb58848feb8dcb29
Antti Tohmo <antti.tohmo@gofore.com>
2018-01-25 12:19:40 +0200

    Ensure that index recreation works in databases where it's already set.

--- src/main/resources/db/migration/V3__duplicate_constraints.sql
+++ src/main/resources/db/migration/V3__duplicate_constraints.sql
@@ -1,4 +1,6 @@
 -- Adding duplicate value index "constraints" to CodeScheme and Code tables
 
+DROP INDEX IF EXISTS code_codevalue_unique_per_codescheme;
+DROP INDEX IF EXISTS codescheme_codevalue_unique_per_coderegistry;
 CREATE UNIQUE INDEX code_codevalue_unique_per_codescheme ON public.code (codevalue, codescheme_id);
 CREATE UNIQUE INDEX codescheme_codevalue_unique_per_coderegistry ON public.codescheme (codevalue, coderegistry_id);
