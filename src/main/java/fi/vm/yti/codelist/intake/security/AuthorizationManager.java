f33317111f048c5639f82b806b57c1c915ad049e
Antti Tohmo <antti.tohmo@gofore.com>
2018-01-25 13:51:04 +0200

    Removing unnecessary public keyword.

--- src/main/java/fi/vm/yti/codelist/intake/security/AuthorizationManager.java
+++ src/main/java/fi/vm/yti/codelist/intake/security/AuthorizationManager.java
@@ -5,6 +5,6 @@ import fi.vm.yti.codelist.common.model.Organization;
 import java.util.Collection;
 
 public interface AuthorizationManager {
-    public boolean canBeModifiedByUserInOrganization(final Collection<Organization> organizations );
-    public boolean isSuperUser();
+    boolean canBeModifiedByUserInOrganization(final Collection<Organization> organizations );
+    boolean isSuperUser();
 }
