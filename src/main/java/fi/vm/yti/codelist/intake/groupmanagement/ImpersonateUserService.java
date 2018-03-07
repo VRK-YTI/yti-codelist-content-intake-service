3dc06ab0c8acddb298803dbcc0cccd26f9e9dfb0
Antti Tohmo <antti.tohmo@gofore.com>
2018-03-07 08:09:23 +0200

    Refactoring to use logger instead of println.

--- src/main/java/fi/vm/yti/codelist/intake/groupmanagement/ImpersonateUserService.java
+++ src/main/java/fi/vm/yti/codelist/intake/groupmanagement/ImpersonateUserService.java
@@ -6,18 +6,22 @@ import java.util.List;
 import javax.inject.Inject;
 import javax.validation.constraints.NotNull;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
 import fi.vm.yti.codelist.intake.configuration.GroupManagementProperties;
 import fi.vm.yti.codelist.intake.configuration.ImpersonateProperties;
-import static fi.vm.yti.codelist.common.constants.ApiConstants.*;
+import static fi.vm.yti.codelist.common.constants.ApiConstants.GROUPMANAGEMENT_API_CONTEXT_PATH;
+import static fi.vm.yti.codelist.common.constants.ApiConstants.GROUPMANAGEMENT_API_USERS;
 import static org.springframework.http.HttpMethod.GET;
 
 @Component
 public class ImpersonateUserService {
 
+    private static final Logger LOG = LoggerFactory.getLogger(ImpersonateUserService.class);
     private final GroupManagementProperties groupManagementProperties;
     private final ImpersonateProperties fakeLoginProperties;
     private final RestTemplate restTemplate;
@@ -34,7 +38,7 @@ public class ImpersonateUserService {
     public List<GroupManagementUser> getUsers() {
         if (fakeLoginProperties.isAllowed()) {
             String url = groupManagementProperties.getUrl() + GROUPMANAGEMENT_API_CONTEXT_PATH + GROUPMANAGEMENT_API_USERS;
-            System.out.println("URL " + url);
+            LOG.info("URL " + url);
             return restTemplate.exchange(url, GET, null, new ParameterizedTypeReference<List<GroupManagementUser>>() {
             }).getBody();
         } else {
