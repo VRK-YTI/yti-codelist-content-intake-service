dd5ffb5ab0853d892afee2a0af34215355dc3216
Antti Tohmo <antti.tohmo@gofore.com>
2018-03-12 10:58:00 +0200

    Changing to explicit query clause in codescheme fetching method.

--- src/main/java/fi/vm/yti/codelist/intake/jpa/CodeRepository.java
+++ src/main/java/fi/vm/yti/codelist/intake/jpa/CodeRepository.java
@@ -5,7 +5,9 @@ import java.util.UUID;
 
 import javax.transaction.Transactional;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import fi.vm.yti.codelist.common.model.Code;
@@ -27,7 +29,8 @@ public interface CodeRepository extends CrudRepository<Code, String> {
 
     Set<Code> findByCodeScheme(final CodeScheme codeScheme);
 
-    Set<Code> findByCodeSchemeIdAndBroaderCodeIdIsNull(final UUID codeSchemeId);
+    @Query(value = "SELECT c FROM Code as c WHERE c.codeScheme.id = :codeSchemeId AND c.broaderCodeId IS NULL ORDER BY c.codeValue DESC")
+    Set<Code> findByCodeSchemeIdAndBroaderCodeIdIsNull(@Param("codeSchemeId") final UUID codeSchemeId);
 
     Set<Code> findAll();
 }
