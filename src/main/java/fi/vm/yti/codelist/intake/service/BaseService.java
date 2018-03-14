4a019c44c7825256e6ddc04ff57474c2c266744f
Antti Tohmo <antti.tohmo@gofore.com>
2018-03-14 15:29:13 +0200

    Adding missing legalBase and governancePolicy setters to DTO mapping.

--- src/main/java/fi/vm/yti/codelist/intake/service/BaseService.java
+++ src/main/java/fi/vm/yti/codelist/intake/service/BaseService.java
@@ -77,6 +77,8 @@ public class BaseService {
         codeSchemeDto.setPrefLabel(codeScheme.getPrefLabel());
         codeSchemeDto.setDescription(codeScheme.getDescription());
         codeSchemeDto.setCodeRegistry(mapCodeRegistryDto(codeScheme.getCodeRegistry()));
+        codeSchemeDto.setGovernancePolicy(codeScheme.getGovernancePolicy());
+        codeSchemeDto.setLegalBase(codeScheme.getLegalBase());
         if (deep) {
             if (codeScheme.getDataClassifications() != null) {
                 codeSchemeDto.setDataClassifications(mapCodeDtos(codeScheme.getDataClassifications(), false));
