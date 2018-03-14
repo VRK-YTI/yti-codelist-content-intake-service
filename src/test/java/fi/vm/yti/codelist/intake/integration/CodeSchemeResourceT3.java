0ebf2d13ddef7dd3e1cac2a8a1de7a54080ba039
Antti Tohmo <antti.tohmo@gofore.com>
2018-03-14 11:04:41 +0200

    Removing unused imports.

--- src/test/java/fi/vm/yti/codelist/intake/integration/CodeSchemeResourceT3.java
+++ src/test/java/fi/vm/yti/codelist/intake/integration/CodeSchemeResourceT3.java
@@ -5,26 +5,17 @@ import javax.transaction.Transactional;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.context.embedded.LocalServerPort;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.util.LinkedMultiValueMap;
 
 import fi.vm.yti.codelist.common.model.CodeRegistry;
 import fi.vm.yti.codelist.intake.ContentIntakeServiceApplication;
 import fi.vm.yti.codelist.intake.jpa.CodeRegistryRepository;
 import fi.vm.yti.codelist.intake.jpa.CodeSchemeRepository;
-import static fi.vm.yti.codelist.common.constants.ApiConstants.*;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
