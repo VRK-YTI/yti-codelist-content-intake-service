fc8f3e25fb4c647e4eb6d1b47750835320cda3b8
Antti Tohmo <antti.tohmo@gofore.com>
2018-05-10 19:56:09 +0300

    Adding logging to uncaught exceptions.

--- src/main/java/fi/vm/yti/codelist/intake/exception/exceptionmapping/UncaughtExceptionMapper.java
+++ src/main/java/fi/vm/yti/codelist/intake/exception/exceptionmapping/UncaughtExceptionMapper.java
@@ -4,6 +4,8 @@ import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.ExceptionMapper;
 import javax.ws.rs.ext.Provider;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 
 import fi.vm.yti.codelist.intake.api.ResponseWrapper;
@@ -13,8 +15,11 @@ import fi.vm.yti.codelist.intake.model.Meta;
 @Provider
 public class UncaughtExceptionMapper implements ExceptionMapper<Exception> {
 
+    private static final Logger LOG = LoggerFactory.getLogger(UncaughtExceptionMapper.class);
+
     @Override
     public Response toResponse(final Exception e) {
+        LOG.warn("Uncaught exception: " + e.getMessage());
         final ResponseWrapper wrapper = new ResponseWrapper();
         final Meta meta = new Meta();
         meta.setMessage(ErrorConstants.ERR_MSG_USER_500);
