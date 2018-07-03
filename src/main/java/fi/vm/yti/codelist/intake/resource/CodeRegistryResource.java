46d2088a5da80ba6d82342fcc5176f7ad59094c5
Antti Tohmo <antti.tohmo@gofore.com>
2018-07-03 11:06:38 +0300

    YTI-871: Registry cannot be deleted if it has code lists, bugfix.

--- src/main/java/fi/vm/yti/codelist/intake/resource/CodeRegistryResource.java
+++ src/main/java/fi/vm/yti/codelist/intake/resource/CodeRegistryResource.java
@@ -164,7 +164,7 @@ public class CodeRegistryResource extends AbstractBaseResource {
     public Response deleteCodeRegistry(@ApiParam(value = "CodeRegistry codeValue", required = true) @PathParam("codeRegistryCodeValue") final String codeRegistryCodeValue) {
 
         final CodeRegistryDTO existingCodeRegistry = codeRegistryService.findByCodeValue(codeRegistryCodeValue);
-        if (existingCodeRegistry.getCodeSchemes() != null && existingCodeRegistry.getCodeSchemes().isEmpty()) {
+        if (existingCodeRegistry.getCodeSchemes() != null && !existingCodeRegistry.getCodeSchemes().isEmpty()) {
             throw new YtiCodeListException(new ErrorModel(HttpStatus.NOT_ACCEPTABLE.value(), ERR_MSG_USER_CODEREGISTRY_NOT_EMPTY));
         }
         if (existingCodeRegistry != null) {
