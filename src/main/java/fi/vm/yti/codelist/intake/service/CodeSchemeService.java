24c07289fefdaf598d26ccdb1359b234209c2781
Antti Tohmo <antti.tohmo@gofore.com>
2018-07-06 18:12:02 +0300

    Minor code style refactor.

--- src/main/java/fi/vm/yti/codelist/intake/service/CodeSchemeService.java
+++ src/main/java/fi/vm/yti/codelist/intake/service/CodeSchemeService.java
@@ -35,6 +35,7 @@ public interface CodeSchemeService {
     CodeSchemeDTO deleteCodeScheme(final String codeRegistryCodeValue,
                                    final String codeSchemeCodeValue);
 
-    CodeSchemeDTO updateCodeSchemeFromDto (String codeRegistryCodeValue, CodeSchemeDTO codeSchemeDTO);
+    CodeSchemeDTO updateCodeSchemeFromDto(String codeRegistryCodeValue,
+                                          CodeSchemeDTO codeSchemeDTO);
 
 }
