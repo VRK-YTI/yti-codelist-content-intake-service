a120e134eb6fba59796d8fb20cd648703faf4a2d
Antti Tohmo <antti.tohmo@gofore.com>
2018-09-10 19:06:24 +0300

    Removing duplicate annotation by configuring generic one.

--- src/main/java/fi/vm/yti/codelist/intake/ContentIntakeServiceApplication.java
+++ src/main/java/fi/vm/yti/codelist/intake/ContentIntakeServiceApplication.java
@@ -4,15 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "fi.vm.yti.codelist.*")
 @EnableJpaRepositories("fi.vm.yti.codelist.*")
 @EnableTransactionManagement
 @EntityScan("fi.vm.yti.codelist.*")
-@ComponentScan({ "fi.vm.yti.codelist.*" })
 public class ContentIntakeServiceApplication {
 
     public static void main(final String[] args) {
