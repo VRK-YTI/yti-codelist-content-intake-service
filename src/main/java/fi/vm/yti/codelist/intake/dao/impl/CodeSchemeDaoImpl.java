66baa15fa35b07fcd2d9047a247e81864500298b
Tuukka Laitinen <work.ictlaitinen@gmail.com>
2019-03-05 14:59:31 +0200

    YTI-366, cumulative codelist can now be marked thru the update page too.

--- src/main/java/fi/vm/yti/codelist/intake/dao/impl/CodeSchemeDaoImpl.java
+++ src/main/java/fi/vm/yti/codelist/intake/dao/impl/CodeSchemeDaoImpl.java
@@ -344,6 +344,7 @@ public class CodeSchemeDaoImpl implements CodeSchemeDao {
         existingCodeScheme.setPrevCodeschemeId(fromCodeScheme.getPrevCodeschemeId());
         existingCodeScheme.setNextCodeschemeId(fromCodeScheme.getNextCodeschemeId());
         existingCodeScheme.setModified(new Date(System.currentTimeMillis()));
+        existingCodeScheme.setCumulative(fromCodeScheme.isCumulative());
 
         return existingCodeScheme;
     }
