bf8aca8bf0660e70b9425732a77d991f8b539809
Tuukka Laitinen <work.ictlaitinen@gmail.com>
2019-03-15 13:00:11 +0200

    YTI-488 , fixing earlier fix, erroneously mixed up code order before.

--- src/main/java/fi/vm/yti/codelist/intake/dao/impl/CodeDaoImpl.java
+++ src/main/java/fi/vm/yti/codelist/intake/dao/impl/CodeDaoImpl.java
@@ -430,8 +430,8 @@ public class CodeDaoImpl implements CodeDao {
             final int order = getNextOrderInSequence(codeScheme);
             code.setOrder(order);
         } else {
-            nextOrder.setValue(nextOrder.getValue() + 1);
             code.setOrder(nextOrder.getValue());
+            nextOrder.setValue(nextOrder.getValue() + 1);
         }
         for (Map.Entry<String, String> entry : fromCode.getPrefLabel().entrySet()) {
             final String language = entry.getKey();
