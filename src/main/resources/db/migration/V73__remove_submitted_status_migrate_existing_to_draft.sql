6f7eb49604fdf37a1e6354c9bb2bf86953f9436a
Tuukka Laitinen <work.ictlaitinen@gmail.com>
2019-05-15 11:51:27 +0300

    YTI-530 , remove SUBMITTED-status from everywhere.

---
+++ src/main/resources/db/migration/V73__remove_submitted_status_migrate_existing_to_draft.sql
@@ -0,0 +1,3 @@
+UPDATE codescheme SET status = 'DRAFT' WHERE status = 'SUBMITTED';
+UPDATE code SET status = 'DRAFT' WHERE status = 'SUBMITTED';
+UPDATE extension SET status = 'DRAFT' WHERE status = 'SUBMITTED';
