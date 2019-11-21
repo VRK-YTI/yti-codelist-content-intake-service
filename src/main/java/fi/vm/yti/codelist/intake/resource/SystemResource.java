c80bd5b0abce63df97e9cc4228a8abfac7d52a13
Antti Tohmo <antti.tohmo@gofore.com>
2019-11-21 16:19:50 +0200

    YTI-1140: Moving /configuration endpoint under system resource.

--- src/main/java/fi/vm/yti/codelist/intake/resource/SystemResource.java
+++ src/main/java/fi/vm/yti/codelist/intake/resource/SystemResource.java
@@ -18,7 +18,11 @@ import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
 import fi.vm.yti.codelist.common.dto.ErrorModel;
+import fi.vm.yti.codelist.intake.api.ApiUtils;
 import fi.vm.yti.codelist.intake.dto.SystemMetaCountDTO;
 import fi.vm.yti.codelist.intake.exception.YtiCodeListException;
 import fi.vm.yti.codelist.intake.jpa.CodeRegistryRepository;
@@ -40,17 +44,20 @@ public class SystemResource implements AbstractBaseResource {
 
     private static final Logger LOG = LoggerFactory.getLogger(SystemResource.class);
 
+    private final ApiUtils apiUtils;
     private final CodeRegistryRepository codeRegistryRepository;
     private final CodeSchemeRepository codeSchemeRepository;
     private final CodeRepository codeRepository;
     private final ExtensionRepository extensionRepository;
     private final MemberRepository memberRepository;
 
-    public SystemResource(final CodeRegistryRepository codeRegistryRepository,
+    public SystemResource(final ApiUtils apiUtils,
+                          final CodeRegistryRepository codeRegistryRepository,
                           final CodeSchemeRepository codeSchemeRepository,
                           final CodeRepository codeRepository,
                           final ExtensionRepository extensionRepository,
                           final MemberRepository memberRepository) {
+        this.apiUtils = apiUtils;
         this.codeRegistryRepository = codeRegistryRepository;
         this.codeSchemeRepository = codeSchemeRepository;
         this.codeRepository = codeRepository;
@@ -79,6 +86,46 @@ public class SystemResource implements AbstractBaseResource {
             countMeta = new SystemMetaCountDTO(codeRegistryRepository.count(), codeSchemeRepository.count(), codeRepository.count(), extensionRepository.count(), memberRepository.count());
             return createCountMetaResponse(countMeta, format);
         }
+    }
+
+    @GET
+    @Path("config")
+    @Produces(MediaType.APPLICATION_JSON + ";charset=UTF-8")
+    @Operation(summary = "Get configuration values as JSON")
+    @ApiResponse(responseCode = "200", description = "Returns the configuration JSON element to the frontend related to this service.")
+    public Response getConfig() {
+        final ObjectMapper mapper = new ObjectMapper();
+        final ObjectNode configJson = mapper.createObjectNode();
+
+        final String groupManagementPublicUrl = apiUtils.getGroupmanagementPublicUrl();
+        final ObjectNode groupManagementConfig = mapper.createObjectNode();
+        groupManagementConfig.put("url", groupManagementPublicUrl);
+        configJson.set("groupManagementConfig", groupManagementConfig);
+
+        final String datamodelPublicUrl = apiUtils.getDataModelPublicUrl();
+        final ObjectNode dataModelConfig = mapper.createObjectNode();
+        dataModelConfig.put("url", datamodelPublicUrl);
+        configJson.set("dataModelConfig", dataModelConfig);
+
+        final String terminologyPublicUrl = apiUtils.getTerminologyPublicUrl();
+        final ObjectNode terminologyConfig = mapper.createObjectNode();
+        terminologyConfig.put("url", terminologyPublicUrl);
+        configJson.set("terminologyConfig", terminologyConfig);
+
+        final String commentsPublicUrl = apiUtils.getCommentsPublicUrl();
+        final ObjectNode commentsConfig = mapper.createObjectNode();
+        commentsConfig.put("url", commentsPublicUrl);
+        configJson.set("commentsConfig", commentsConfig);
+
+        final boolean messagingEnabled = apiUtils.getMessagingEnabled();
+        final ObjectNode messagingConfig = mapper.createObjectNode();
+        messagingConfig.put("enabled", messagingEnabled);
+        configJson.set("messagingConfig", messagingConfig);
+
+        configJson.put("env", apiUtils.getEnv());
+        configJson.put("defaultStatus", apiUtils.getDefaultStatus());
+        configJson.put("codeSchemeSortMode", apiUtils.getCodeSchemeSortMode());
+        return Response.ok(configJson).build();
     }
 
     private Date parseDateFromString(final String dateString) {
