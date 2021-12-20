eb99b762e1ed9c1c992a4fe3447b53cc6dcdcbd4
Kalle Kyttälä <kalle.kyttala@rebase.fi>
2021-12-20 12:47:22 +0200

    use extendwith

--- src/test/java/fi/vm/yti/codelist/intake/unit/AuthorizationManagerTest.java
+++ src/test/java/fi/vm/yti/codelist/intake/unit/AuthorizationManagerTest.java
@@ -1,34 +1,51 @@
 package fi.vm.yti.codelist.intake.unit;
 
 import fi.vm.yti.codelist.common.model.Status;
-import fi.vm.yti.codelist.intake.model.Code;
 import fi.vm.yti.codelist.intake.model.CodeScheme;
 import fi.vm.yti.codelist.intake.model.Organization;
+import fi.vm.yti.codelist.intake.security.AuthorizationManager;
 import fi.vm.yti.codelist.intake.security.AuthorizationManagerImpl;
 import fi.vm.yti.security.AuthenticatedUserProvider;
 import fi.vm.yti.security.Role;
 import fi.vm.yti.security.YtiUser;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 
 import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import javax.inject.Inject;
 import java.util.*;
 import java.util.stream.Collectors;
 
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
 
-@RunWith(MockitoJUnitRunner.class)
+// @RunWith(MockitoJUnitRunner.class)
+
+
+// @ExtendWith(MockitoExtension.class)
+@ExtendWith(SpringExtension.class)
+@Import({
+        AuthenticatedUserProvider.class,
+        AuthorizationManagerImpl.class
+})
 public class AuthorizationManagerTest {
 
-    @Mock
+    // @Mock
+    @MockBean
     AuthenticatedUserProvider provider;
 
-    @InjectMocks
+    // @InjectMocks
+    @Autowired
     AuthorizationManagerImpl authorizationManager;
 
     private static UUID userOrganization = UUID.fromString("d18fe638-d7d7-486d-90b4-73f564328fc4");
